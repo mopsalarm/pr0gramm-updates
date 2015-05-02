b2176088164b6a6ad30588909c6f4cefeca89fe0
Mopsalarm <mopsalarm@example.com>
2015-05-02 15:08:23 +0200

    Go with rawgit.com for update json

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 function getCurrentVersion(callback) {
-	var url = "https://github.com/mopsalarm/pr0gramm-updates/raw/master/update.json";
+	var url = "https://rawgit.com/mopsalarm/pr0gramm-updates/master/update.json";
 	jQuery.getJSON(url).done(function(json) {
 		callback(json.apk);
 	});
