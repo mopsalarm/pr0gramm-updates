108b9aafa72cfc8c13f427f76f0e9cb313ec69a8
Mopsalarm <mopsalarm@example.com>
2015-05-02 17:18:39 +0200

    Modified the update.js for @schr3i

--- update.js
+++ update.js
@@ -1,6 +1,12 @@
-function getCurrentVersion(callback) {
+function getUpdateJson(callback) {
 	var url = "https://rawgit.com/mopsalarm/pr0gramm-updates/master/update.json";
 	jQuery.getJSON(url).done(function(json) {
+		callback(json);
+	});
+}
+
+function getCurrentVersion(callback) {
+	getUpdateJson(function(json) {
 		callback(json.apk);
 	});
 }
