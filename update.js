33b082e789fdb0a6a391cb7d54e24539b3c45202
schr3j <schr3j@users.noreply.github.com>
2015-11-07 13:06:56 +0100

    fix url

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 function getUpdateJson(callback) {
-	var url = "https://rawgit.com/mopsalarm/pr0gramm-updates/master/update.json";
+	var url = "https://rawgit.com/mopsalarm/pr0gramm-updates/master/open/update.json";
 	jQuery.getJSON(url).done(function(json) {
 		callback(json);
 	});
